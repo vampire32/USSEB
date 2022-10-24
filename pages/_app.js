fac0d036aca3160ed7753015fb74c8f48fe0e9f5
vampire32 <moizabdul320@gmail.com>
2022-10-24 15:38:14 +0500

    navbar changes

--- pages/_app.js
+++ pages/_app.js
@@ -45,15 +45,20 @@ function MyApp({ Component, pageProps }) {
 					rel="stylesheet"
 					integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
 					crossOrigin="anonymous"
-				></link>
+				/>
 			</Head>
 			<Script src="https://cdn.tailwindcss.com" />
 			<Script src="https://smtpjs.com/v3/smtp.js" />
 
 			<Script
-				src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-				integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+				src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+				integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
 				crossOrigin="anonymous"
+			></Script>
+			<Script
+				src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+				integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+				crossorigin="anonymous"
 			></Script>
 
 			{loading ? (
